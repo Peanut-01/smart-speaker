ef8d070c5f0a5685aa9e508a3a4f421912f65fc2
zch <935251327@qq.com>
2026-09-06 02:53:02 +0000

    模型初始化

--- asr/main.c
+++ asr/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "alsa.h"
+#include "sherpa.h"
 
 
 int main()
@@ -11,6 +12,14 @@ int main()
         return -1;
     }
     printf("ALSA初始化成功\n");
-    
+
+    // 初始化语音识别
+    if (init_sherpa_asr() == -1)
+    {
+        printf("语音识别初始化失败\n");
+        return -1;
+    }
+    printf("语音识别初始化成功\n");
+
     return 0;
 }
